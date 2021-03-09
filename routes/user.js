afd04a5bc23776820c3bd5d6e1bb9921a5456601
mbrown <tugtuj@gmail.com>
2021-03-09 16:51:05 -0500

    router update

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 const passport = require('passport');
 
+router.get('/', ctrl.user.index);
+router.get('/:id', ctrl.user.show);
+router.put('/:id', ctrl.user.update);
 router.get('/test', ctrl.user.test);
 router.post('/register', ctrl.user.register);
 router.post('/login', ctrl.user.login);
