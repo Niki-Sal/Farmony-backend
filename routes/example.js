3572a90fc1a922462fbc95358750cbaccc2bb83f
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-03-09 16:14:14 -0800

    added query controller and route

--- routes/example.js
+++ routes/example.js
@@ -4,6 +4,7 @@ const ctrl = require('../controllers');
 // routes
 router.get('/', ctrl.example.index);
 router.get('/:id', ctrl.example.show);
+router.post('/query', ctrl.example.query);
 router.post('/', ctrl.example.create);
 router.put('/:id', ctrl.example.update);
 router.delete('/:id', ctrl.example.destroy);
