fad9d791b1319f98e570d1e849eee60a791350e1
mbrown <tugtuj@gmail.com>
2021-03-10 23:36:26 -0500

    File add

---
+++ controllers/trade.js
@@ -0,0 +1,32 @@
+const db = require('../models')
+
+const index = (req, res) => {
+    console.log('=====> Displaying Posts')
+    db.Post.find({
+        category: 'Trade'
+    }, 
+    (err, foundPosts) => {
+        if (err) console.log('Error in trade#index:', err)
+        res.json(foundPosts)
+    })
+}
+
+const create = (req, res) => {
+    console.log('=====> Post Created')
+    db.Post.create({
+        title: req.body.title,
+        name: req.user.name,
+        content: req.body.content,
+        category: req.body.category,
+        postType: req.body.postType,
+        date: Date(),
+    },(err, postCreated) => {
+        if (err) console.log('Error in trade#create', err)
+        res.json(postCreated)
+    })
+}
+
+module.exports = {
+    index,
+    create
+};
