d62478f2c8d8d560cec8c8a06bae1484ecf20c5f
mbrown <tugtuj@gmail.com>
2021-03-10 21:45:49 -0500

    Middleware add

--- routes/holistic.js
+++ routes/holistic.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const ctrl = require('../controllers')
+const passport = require('passport')
 
 router.get('/', ctrl.holistic.index)
+router.post('/', passport.authenticate('jwt', { session: false }), ctrl.holistic.create)
 
 module.exports = router;
