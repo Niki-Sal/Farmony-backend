fe5f66048c85f36c408ec6293c92165c98b01b78
mbrown <tugtuj@gmail.com>
2021-03-13 18:17:01 -0500

    Add put/show

--- routes/trade.js
+++ routes/trade.js
@@ -3,6 +3,8 @@ const ctrl = require('../controllers')
 const passport = require('passport')
 
 router.get('/', ctrl.trade.index)
+router.get('/:id', ctrl.trade.show)
 router.post('/', passport.authenticate('jwt', { session: false }), ctrl.trade.create)
+router.put('/:id', ctrl.trade.update);
 
 module.exports = router;
