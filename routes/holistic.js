85d557e7a7b752e5fc09ce4194458bebc3f3bf10
mbrown <tugtuj@gmail.com>
2021-03-13 18:17:30 -0500

    Add update/show

--- routes/holistic.js
+++ routes/holistic.js
@@ -4,5 +4,7 @@ const passport = require('passport')
 
 router.get('/', ctrl.holistic.index)
 router.post('/', passport.authenticate('jwt', { session: false }), ctrl.holistic.create)
+router.get('/:id', ctrl.holistic.show)
+router.put('/:id', ctrl.holistic.update);
 
 module.exports = router;
