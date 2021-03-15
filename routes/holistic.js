6586cd09d20e50036f42429fc7f781f077d2b8ab
niki-sal <ne.salehi@gmail.com>
2021-03-14 21:20:55 -0700

    Add routes to holistic.js at routes

--- routes/holistic.js
+++ routes/holistic.js
@@ -3,6 +3,8 @@ const ctrl = require('../controllers')
 const passport = require('passport')
 
 router.get('/', ctrl.holistic.index)
+router.get('/:id', ctrl.holistic.show)
 router.post('/', passport.authenticate('jwt', { session: false }), ctrl.holistic.create)
+router.put('/:id', ctrl.holistic.update);
 
 module.exports = router;
